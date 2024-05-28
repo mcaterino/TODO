17d93392cf3f6b55bee41bf125ce76376f7b03aa
Mauricio Caterino <mauricio_caterino@outlook.com>
2024-05-28 10:31:22 -0400

    Created models for the API

---
+++ ToDoAPI/Models/DbSettings.cs
@@ -0,0 +1,7 @@
+namespace ToDoAPI.Models
+{
+    public class DbSettings
+    {
+        public string? ConnectionString { get; set; }
+    }
+}
---
+++ ToDoAPI/Models/ErrorResponse.cs
@@ -0,0 +1,9 @@
+namespace ToDoAPI.Models
+{
+    public class ErrorResponse
+    {
+      public string? Title { get; set; }
+      public int StatusCode { get; set; }
+      public string? Message { get; set; }
+    }
+}
