b6aec1bec6d496320e4db73040cb8bfa563423b7
Mauricio Caterino <mauricio_caterino@outlook.com>
2024-05-28 11:24:01 -0400

    Registered AppDbContext for DI

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,3 +1,6 @@
+using ToDoAPI.Data;
+using ToDoAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DbSettings>(
+    builder.Configuration.GetSection(nameof(DbSettings)));
+
+builder.Services.AddSingleton<AppDbContext>();
 
 var app = builder.Build();
 
